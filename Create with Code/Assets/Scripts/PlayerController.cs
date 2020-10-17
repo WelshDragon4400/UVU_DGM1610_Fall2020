2eb4ede859400dad95ac29d1c3e3e5348c97cf5f
Caoilfhionn <welshdragon4400@gmail.com>
2020-10-17 15:35:47 -0600

    prototype one added more obstacles and inputs so the player can control the vehicle

--- Create with Code/Assets/Scripts/PlayerController.cs
+++ Create with Code/Assets/Scripts/PlayerController.cs
@@ -4,17 +4,25 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public int speed = 20;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float speed = 20f;
+
+    public float turnSpeed = 45f;
+
+    public float horizontalInput;
+
+    public float forwardInput;
+    
 
     // Update is called once per frame
     void Update()
     {
-        //move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        //get forward and horizontal axis for variables
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        
+        //move the vehicle forward based on vertical input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        //rotates the car based on horizontal input
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
