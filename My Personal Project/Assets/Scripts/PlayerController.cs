6aee2460a3988f4275006dbf43d83abf42d279aa
Caoilfhionn <welshdragon4400@gmail.com>
2020-12-15 17:28:55 -0700

    added player movements and constraints to personal project. added camera follow

---
+++ My Personal Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed = 10.0f;
+    private Rigidbody playerRB;
+    
+    
+    public float turnSpeed = 45f;
+
+    public float horizontalInput;
+
+    public float forwardInput;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       /* float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        playerRB.AddForce(Vector3.forward * speed * horizontalInput);
+        playerRB.AddForce(Vector3.right * speed * verticalInput);*/
+          
+          
+          
+          
+          
+//get forward and horizontal axis for variables
+            horizontalInput = Input.GetAxis("Horizontal");
+            forwardInput = Input.GetAxis("Vertical");
+        
+            //move the player forward based on vertical input
+            transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+            //rotates the player based on horizontal input
+            transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+    }
+}
